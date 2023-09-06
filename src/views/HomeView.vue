ffd88e73be24ebe599dbdf9681c9c23a4e571d93
satria1697 <andhikasatriab@gmail.com>
2023-09-06 15:51:14 +0700

    [feat] add navigation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
-    <router-link :to="{ name: 'normal-to-camel' }">Normal to Camel View</router-link>
-    <router-link :to="{ name: 'normal-to-class' }">Normal to Class View</router-link>
+  <div class="h-screen">
+    <div class="flex flex-col space-y-10 items-center justify-center w-full h-full">
+      <router-link
+        class="rounded-md px-2.5 py-1.5 bg-green-600 text-white w-fit"
+        :to="{ name: 'normal-to-camel' }"
+        >Normal to Camel View</router-link
+      >
+      <router-link
+        class="rounded-md px-2.5 py-1.5 bg-green-600 text-white w-fit"
+        :to="{ name: 'normal-to-class' }"
+        >Normal to Class View</router-link
+      >
+    </div>
   </div>
 </template>
 <script setup lang="ts"></script>
--- src/views/NormalToClassView.vue
+++ src/views/NormalToClassView.vue
@@ -3,18 +3,17 @@ import { reactive } from 'vue'
 import MiButton from '@/components/global/MiButton.vue'
 import _ from 'lodash'
 import MiToast from '@/components/global/mi-toast'
-import { DOMElements, ElementSelector } from '@/entities/json-element'
+import { DOMElementSelector, ElementSelector } from '@/entities/json-element'
+
 const miToast = new MiToast()
 
 interface reactiveInterface {
   text: string
-  title: string
   converted: string
   elementSelector: ElementSelector
 }
 const data = reactive<reactiveInterface>({
   text: '',
-  title: '',
   converted: '',
   elementSelector: {
     isUpdated: true,
@@ -25,14 +24,18 @@ const data = reactive<reactiveInterface>({
   },
 })
 
+const checkValue = (element: DOMElementSelector) => {
+  return `\t return cy.get('#${element.id ?? ''}')\n`
+}
+
 const convertObject = (elementSelector: ElementSelector) => {
   data.converted = ''
-  data.converted += `class ${_.camelCase(elementSelector.pageName)} {\n`
+  data.converted += `class ${_.upperFirst(_.camelCase(elementSelector.pageName))} {\n`
   for (const domElement in elementSelector.domElements) {
     const element = elementSelector.domElements[domElement]
 
     data.converted += `${_.camelCase(domElement)}() {\n`
-    data.converted += `\t return cy.get('#${element.id ?? ''}')\n`
+    data.converted += checkValue(element)
     data.converted += '}\n'
     data.converted += '\n'
   }
@@ -64,7 +67,6 @@ const copyData = () => {
 
 <template>
   <div class="flex flex-col space-y-4 p-10">
-    <input v-model="data.title" class="border rounded-md" />
     <textarea v-model="data.text" rows="5" class="border rounded-md"></textarea>
     <mi-button text="convert" @click="convertObject(toJson(data.text))" />
     <mi-button text="copy" @click="copyData()" />
